e6f78e3c905e6da05f1494b142cb0fc074ceb784
chdonncha@gmail.com <T3CUmI4GNUa1>
2022-04-01 22:53:42 +0100

    git init + angular material import

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import {MatSliderModule} from '@angular/material/slider';
+import { MaterialModule } from './material/material.module';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        // MatSliderModule,
+        MaterialModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
