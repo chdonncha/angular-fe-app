34e01b271d0fa0fe644b651cd9eaef39f501c754
donncha <chdonncha@gmail.com>
2022-04-05 21:59:00 +0100

    base table functionality

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -5,55 +5,42 @@ import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'exads-users',
-  template: `
-          <p><button mat-raised-button color="primary">New User</button>
-          </p>
-          <div *ngFor="let user of users | async | keyvalue">
-              <ul>
-                  <li>Name: {{ user.value.first_name }} </li>
-                  <li>Email: {{ user.value.email }}</li>
-              </ul>
-          </div>
+  template: `      
+      <table mat-table [dataSource]="this.users" class="mat-elevation-z8">-->
 
-<!--      <table mat-table [dataSource]="dataSource" class="mat-elevation-z8">-->
+          <!-- Username -->
+          <ng-container matColumnDef="position">
+              <th mat-header-cell *matHeaderCellDef> Username</th>
+              <td mat-cell *matCellDef="let element"> {{element.username}} </td>
+          </ng-container>
 
-<!--          &lt;!&ndash; Username Column &ndash;&gt;-->
-<!--          <ng-container matColumnDef="position">-->
-<!--              <th mat-header-cell *matHeaderCellDef> Username</th>-->
-<!--              <td mat-cell *matCellDef="let element"> {{element.first_name}} </td>-->
-<!--          </ng-container>-->
-
-<!--          &lt;!&ndash; Full Name Column &ndash;&gt;-->
-<!--          <ng-container matColumnDef="name">-->
-<!--              <th mat-header-cell *matHeaderCellDef> Full Name</th>-->
-<!--              <td mat-cell *matCellDef="let element"> {{element.email}} </td>-->
-<!--          </ng-container>-->
+          <!-- Full Name -->
+          <ng-container matColumnDef="name">
+              <th mat-header-cell *matHeaderCellDef> Full Name</th>
+              <td mat-cell *matCellDef="let element"> {{element.first_name}} {{ element.last_name}} </td>
+          </ng-container>
 
           <!-- Email Column -->
-<!--          <ng-container matColumnDef="weight">-->
-<!--              <th mat-header-cell *matHeaderCellDef> Email</th>-->
-<!--              <td mat-cell *matCellDef="let element"> {{element.weight}} </td>-->
-<!--          </ng-container>-->
+          <ng-container matColumnDef="weight">
+              <th mat-header-cell *matHeaderCellDef> Email</th>
+              <td mat-cell *matCellDef="let element"> {{element.email}} </td>
+          </ng-container>
 
           <!-- Status Column -->
-<!--          <ng-container matColumnDef="symbol">-->
-<!--              <th mat-header-cell *matHeaderCellDef> Status</th>-->
-<!--              <td mat-cell *matCellDef="let element"> {{element.symbol}} </td>-->
-<!--          </ng-container>-->
+          <ng-container matColumnDef="symbol">
+              <th mat-header-cell *matHeaderCellDef> Status</th>
+              <td mat-cell *matCellDef="let element"> {{element.created_date}} </td>
+          </ng-container>
 
-          <!-- Date Created Column -->
-<!--          <ng-container matColumnDef="symbol">-->
-<!--              <th mat-header-cell *matHeaderCellDef> Date Created</th>-->
-<!--              <td mat-cell *matCellDef="let element"> {{element.symbol}} </td>-->
-<!--          </ng-container>-->
-
-<!--          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>-->
-<!--          <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>-->
-<!--      </table>-->
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+      </table>
   `,
   styles: []
 })
 export class UsersComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = "test";
   users: any;
 
   constructor(private apiService: ApiService) {}
@@ -62,15 +49,16 @@ export class UsersComponent implements OnInit {
 
     this.getUserData();
     console.log(this.users);
+    
     // console.log(data);
     // this.populateTable();
 
     // console.log(dataSource);
   }
 
-  populateTable() {
-    // this.dataSource = new MatTableDataSource();
-  }
+  // populateTable() {
+  //   this.dataSource = new MatTableDataSource();
+  // }
 
   getUserData() {
     this.users = this.apiService.getUsers();
