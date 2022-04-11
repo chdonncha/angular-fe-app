f228e1a3b5a3f8af45a695026cfb0d5b5ba38190
donncha <chdonncha@gmail.com>
2022-04-11 23:40:19 +0100

    template cleanup

--- src/app/pages/users-create/users-create.component.ts
+++ src/app/pages/users-create/users-create.component.ts
@@ -4,61 +4,7 @@ import { FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'exads-users-create',
-  template: `
-      <mat-card>
-          <mat-card-title>
-              Add New User
-          </mat-card-title>
-          <mat-card-content>
-              <form>
-                  <table>
-                      <tr>
-                          <mat-form-field appearance="fill">
-                              <mat-label>Username</mat-label>
-                              <input matInput>
-                          </mat-form-field>
-                      </tr>
-                      <tr>
-                          <td>
-                              <mat-form-field appearance="fill">
-                                  <mat-label>First Name</mat-label>
-                                  <input matInput>
-                              </mat-form-field>
-                          </td>
-                          <td>
-                              <mat-form-field appearance="fill">
-                                  <mat-label>Last Name (Optional)</mat-label>
-                                  <input matInput>
-                              </mat-form-field>
-                          </td>
-                      </tr>
-                      <tr>
-                          <mat-form-field appearance="fill">
-                              <input matInput>
-                              <mat-label>Email</mat-label>
-                              <input matInput>
-                          </mat-form-field>
-                      </tr>
-                      <tr>
-                          <mat-card-actions fxLayout="row" fxLayoutAlign="end center">
-                          </mat-card-actions>
-                          <mat-grid-list cols="4" rowHeight="50px">
-                              <mat-grid-tile>
-                                  <button mat-raised-button>CANCEL</button>
-                              </mat-grid-tile>
-                              <mat-grid-tile></mat-grid-tile>
-                              <mat-grid-tile>
-<!--                                  <button mat-raised-button type="button" (click)="createUser($event)">ADD NEW USER</button>-->
-                                  <button mat-raised-button type="submit" (click)="createUser($event)">ADD NEW USER</button>
-                              </mat-grid-tile>
-                              <mat-grid-tile></mat-grid-tile>
-                          </mat-grid-list>
-                      </tr>
-                  </table>
-              </form>
-          </mat-card-content>
-      </mat-card>
-  `,
+  templateUrl: `./users-create.component.html`,
   styles: []
 })
 export class UsersCreateComponent implements OnInit {
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -5,60 +5,7 @@ import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'exads-users',
-  template: `
-      <mat-card>
-          <mat-card-content>
-              <table>
-                  <tr>
-                      <button mat-raised-button color="primary" routerLink="/users/create">Create User</button>
-                  </tr>
-                  <tr>
-                      <table mat-table [dataSource]="dataSource" class="mat-elevation-z8 demo-table">
-
-                          <mat-paginator #paginator [pageSizeOptions]="[2, 4, 6]"
-                                         showFirstLastButtons></mat-paginator>
-
-                          <!-- Username -->
-                          <ng-container matColumnDef="username">
-                              <th mat-header-cell *matHeaderCellDef> Username</th>
-                              <td mat-cell *matCellDef="let element"> {{element.username}} </td>
-                          </ng-container>
-
-                          <!-- Full Name -->
-                          <ng-container matColumnDef="full_name">
-                              <th mat-header-cell *matHeaderCellDef> Full Name</th>
-                              <td mat-cell
-                                  *matCellDef="let element"> {{element.first_name}} {{ element.last_name}} </td>
-                          </ng-container>
-
-                          <!-- Email Column -->
-                          <ng-container matColumnDef="email">
-                              <th mat-header-cell *matHeaderCellDef> Email</th>
-                              <td mat-cell *matCellDef="let element"> {{element.email}} </td>
-                          </ng-container>
-
-                          <!-- Status Column -->
-                          <ng-container matColumnDef="created_date">
-                              <th mat-header-cell *matHeaderCellDef> Status</th>
-                              <td mat-cell *matCellDef="let element"> {{element.created_date}} </td>
-                          </ng-container>
-
-                          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-                          <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
-                      </table>
-                  </tr>
-              </table>
-          </mat-card-content>
-      </mat-card>
-      <div *ngIf="isLoading"
-           style="display: flex; justify-content: center; align-items: center; background: white;">
-          <mat-progress-spinner
-                  color="primary"
-                  mode="indeterminate">
-          </mat-progress-spinner>
-      </div>
-
-  `,
+  templateUrl: `./users.component.html`,
   styles: []
 })
 export class UsersComponent implements OnInit {
