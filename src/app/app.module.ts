b8752664e8782d98e7a270100f2ae15d3f7d5492
donncha <chdonncha@gmail.com>
2022-04-12 22:26:40 +0100

    Basic form submit and post setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 
 @NgModule({
@@ -19,6 +20,8 @@ import { MaterialModule } from './material/material.module';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/users-create/users-create.component.ts
+++ src/app/pages/users-create/users-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../../services/api.service";
-import { FormControl, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'exads-users-create',
@@ -14,22 +14,47 @@ export class UsersCreateComponent implements OnInit {
   params: any;
   formGroup: any;
 
-  constructor(private apiService: ApiService) {
+  constructor(
+    private apiService: ApiService,
+    private formBuilder: FormBuilder,
+    // public first_name: string,
+    // public last_name: string,
+    // public email: string,
+    // public username: string,
+    // public id_status: number
+  ) {
+    this.formGroup = this.formBuilder.group({
+      username: '',
+      first_name: '',
+      last_name: '',
+      email: '',
+    });
   }
 
   ngOnInit() {
   }
 
-  email= new FormControl('',[
-    Validators.required,
-    Validators.email
-  ]);
+  // email= new FormControl('',[
+  //   Validators.required,
+  //   Validators.email
+  // ]);
 
-  createUser(event) {
+  onSubmit(formData) {
     this.url = 'http://localhost:3000/users';
     this.params = {
-
+      "user": {
+        username: formData['username'],
+        first_name: formData['first_name'],
+        last_name: formData['last_name'],
+        email: formData['email'],
+        id_status: 1,
+      }
     };
+
+    this.users = this.apiService.createUser(this.url, this.params);
+
+
+    // console.log(formData);
 
     // this.userExample = {
     //   "user": {
@@ -41,10 +66,6 @@ export class UsersCreateComponent implements OnInit {
     //   }
     // };
 
-    this.users = this.apiService.createUser(this.url, this.userExample);
-
-    console.log(this.users);
-    console.log("howya");
   }
 
 }
